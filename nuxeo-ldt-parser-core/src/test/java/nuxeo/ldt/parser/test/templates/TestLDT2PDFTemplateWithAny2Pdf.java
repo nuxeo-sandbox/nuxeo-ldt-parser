879d5a0e8b18ce345f51a0d3810803a8c3ff4464
Thibaud Arguillere <thibaud.arguillere@hyland.com>
2025-01-07 15:01:57 +0100

    Give up. Add a test for any2pdf

--- nuxeo-ldt-parser-core/src/test/java/nuxeo/ldt/parser/test/templates/TestLDT2PDFTemplateWithAny2Pdf.java
+++ nuxeo-ldt-parser-core/src/test/java/nuxeo/ldt/parser/test/templates/TestLDT2PDFTemplateWithAny2Pdf.java
@@ -20,6 +20,7 @@
 package nuxeo.ldt.parser.test.templates;
 
 import org.junit.Assert;
+import org.junit.Assume;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.nuxeo.common.utils.FileUtils;
@@ -29,6 +30,7 @@ import org.nuxeo.ecm.automation.test.AutomationFeature;
 import org.nuxeo.ecm.core.api.Blob;
 import org.nuxeo.ecm.core.api.CoreSession;
 import org.nuxeo.ecm.core.api.impl.blob.FileBlob;
+import org.nuxeo.ecm.core.convert.api.ConversionService;
 import org.nuxeo.runtime.test.runner.Deploy;
 import org.nuxeo.runtime.test.runner.Features;
 import org.nuxeo.runtime.test.runner.FeaturesRunner;
@@ -46,17 +48,24 @@ import java.io.File;
         // The automation chain,
         "nuxeo.ldt.parser.nuxeo-ldt-parser-core:render-pdf-with-template/with-any2pdf/automation-render-pdf-with-any2pdf.xml"
 })
-//any2pdf is a a converter provided by the platform out of the box
+//any2pdf is a a converter provided by the platform out of the box. 
+// It requires LibreOffice and its soffic e command line to be available
 public class TestLDT2PDFTemplateWithAny2Pdf {
 
     @Inject
     protected AutomationService automationService;
 
     @Inject
-    CoreSession session;
+    protected CoreSession session;
+    
+    @Inject
+    protected ConversionService conversionService;
 
     @Test
     public void testRenderTemplate() throws Exception {
+        
+        boolean available = conversionService.isConverterAvailable("any2pdf").isAvailable();
+        Assume.assumeTrue("any2pdf not available (soffice not available). Not doing the test", available);
         
         File f = FileUtils.getResourceFileFromContext("files/record.json");
         Blob blob = new FileBlob(f,"application/json");
